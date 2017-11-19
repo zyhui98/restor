a4da7de1c818d4744a409d3963b87d9f2defddd9
zyhui98 <zyhui98@qq.com>
2017-11-19 11:33:07 +0800

    测试类

--- restor-client/src/test/java/com/cditie/restor/restor_client/Java8Test.java
+++ restor-client/src/test/java/com/cditie/restor/restor_client/Java8Test.java
@@ -1,11 +1,18 @@
 package com.cditie.restor.restor_client;
 
+import java.util.Arrays;
+
 /**
  * @author zhuyunhui
  * @since 10/30/2017
  */
 public class Java8Test {
 	public static void main(String[] args) {
-
+		int[] numbers = {1,1,2,2,2,2,2,2,2};
+		int x= Arrays.stream(numbers).reduce(0,(a,b)-> a+b);
+		System.out.println(x);
+		Arrays.stream(numbers).forEach(a->{
+			System.out.println(a);
+		});
 	}
 }
--- restor-client/src/test/java/com/cditie/restor/restor_client/TelnetTest.java
+++ restor-client/src/test/java/com/cditie/restor/restor_client/TelnetTest.java
@@ -10,27 +10,37 @@ import java.net.Socket;
 public class TelnetTest
 {
 	public static void main(String[] args) {
+
 		Socket server = null;
 		try {
 			server = new Socket();
-			InetSocketAddress address = new InetSocketAddress("127.0.0.1",20880);
+			InetSocketAddress address = new InetSocketAddress("192.168.25.85",20880);
 			server.connect(address, 5000);
+
 			//invoke queryById(10)
-			server.getOutputStream().write("invoke queryById(10)  \n".getBytes());
+			String cmd = "count MyService \n";
+			server.getOutputStream().write( cmd.getBytes());
 			//byte[] b =new byte[2048];
 			//server.getInputStream().read(b);
 
-			InputStreamReader inputStreamReader = new InputStreamReader(server.getInputStream(),System.getProperty("sun.jnu.encoding"));
-			String line = "";
+			InputStreamReader inputStreamReader = new InputStreamReader(server.getInputStream(),"GBK");
+			String result = "";
 			char[] b = new char[1];
 			while (inputStreamReader.read(b) != -1){
-				line += new String(b);
-				if(line.endsWith("dubbo>")){
-					line = line.substring(0,line.lastIndexOf("dubbo>"));
-					break;
+				result += new String(b);
+				if(cmd.toLowerCase().startsWith("count")){
+					if(result.endsWith("telnet>")){
+						result = result.substring(0,result.lastIndexOf("telnet>"));
+						break;
+					}
+				}else{
+					if(result.endsWith("dubbo>")){
+						result = result.substring(0,result.lastIndexOf("dubbo>"));
+						break;
+					}
 				}
 			}
-			System.out.println(line);
+			System.out.println(result);
 
 		} catch (Exception e) {
 			System.out.println("telnet失败" + e.getMessage());
